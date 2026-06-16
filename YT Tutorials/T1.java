2e5271ccfe83722b47532bc1ee9579c67e8576b4
Frenz <fccuarteros070813@gmail.com>
2026-06-16 20:59:33 +0800

    First Java Tutorial

--- YT Tutorials/T1.java
+++ YT Tutorials/T1.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
 public class T1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter your name neg...: ");
-        String name = input.nextLine();
-
-        System.out.println("Hello, " + name + "!");
+    public static void main(String[] args){
+        System.out.println("I am Frenz Carlo C. Cuarteros");
+        System.out.println("I am 18 years old");
+        System.out.println("I'll be having my 19th birthday on June 20");
     }
 }
