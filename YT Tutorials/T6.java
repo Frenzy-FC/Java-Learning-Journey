3cd546d3bf75ebf0b76d88c06af1d2570d00b6e8
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-17 14:07:08 +0800

    Add T6: Math Class logics/functions

---
+++ YT Tutorials/T6.java
@@ -0,0 +1,19 @@
+public class T6 {
+    public static void main(String[] args){
+        // Math Class: Logic and Functions
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result;
+        result = Math.pow(1, 6);
+        result = Math.abs(-1);
+        result = Math.sqrt(10);
+        result = Math.round(3.15);
+        result = Math.ceil(3.15);
+        result = Math.floor(3.15);
+        result = Math.max(1, 8);
+        result = Math.min(1, 8);
+
+        System.out.println(result);
+    }
+}
