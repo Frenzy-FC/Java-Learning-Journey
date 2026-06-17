b06add0ad3d4bdd5f0eb72cf4862f972b68063d5
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-17 15:13:35 +0800

    Add T7: Format Specified (printf())

---
+++ YT Tutorials/T7.java
@@ -0,0 +1,17 @@
+public class T7 {
+    public static void main(String[] args){
+        // Printf(): Logic & Functions
+
+        int age = 8;
+        double price = 19.50;
+        char symbol = 'P';
+        String name = "Frenz";
+        boolean isGay = false;
+
+        System.out.printf("My age is %d\n", age);
+        System.out.printf("The price of that product is %.2f\n", price);
+        System.out.printf("The symbol of that currency is %c\n", symbol);
+        System.out.printf("My name is %s\n", name);
+        System.out.printf("Am I gay?: %b\n", isGay);
+    }
+}
