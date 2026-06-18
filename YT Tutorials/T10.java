930ef4cc28d154fd4a74f2abc5523985c5af5311
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-19 07:08:34 +0800

    Add T10: Substring Methods

---
+++ YT Tutorials/T10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class T10 {
+    public static void main(String[] args){
+        // Substring Method Logic/Functions
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your gmail account: ");
+        email = scanner.nextLine();
+
+        username = email.substring(0, email.indexOf("@"));
+        domain = email.substring(email.indexOf("@"));
+
+        System.out.printf("Your email is %s, by slicing your username is %s and your domain is %s", email, username, domain);
+
+
+
+        scanner.close();
+    }
+}
