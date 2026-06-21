06efadc22c61d40e6feea84afaadd7d519639196
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-21 20:56:42 +0800

    Add T15: For Loop: Logic

---
+++ YT Tutorials/T15.java
@@ -0,0 +1,13 @@
+public class T15 {
+    public static void main(String[] args) throws InterruptedException{
+        // For Loop: Logic and Functions
+        // Syntax: for(statement1; statment2; incre/decre){action}
+
+        int end = 20;
+        for(int i =0; i <= end; i++){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+    }
+}
