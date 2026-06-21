bb2a66025f6e4c6d986b177e4e4935e50cf786d9
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-21 21:58:25 +0800

    Add T18: Method: Logic & Functions

---
+++ YT Tutorials/T18.java
@@ -0,0 +1,11 @@
+public class T18 {
+    public static void main(String[] args){
+        // Method: Logic & Functions
+
+        String name = fullName("Frenz Carlo", "Cuarteros");
+        System.out.println(name);
+    }
+    static String fullName(String first, String last){
+        return first + " " + last;
+    }
+}
