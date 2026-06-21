a1c2a89f711f1707950fc7f1502b060b78f27b5d
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-21 21:29:30 +0800

    Add T17: Nested Loop Logic

---
+++ YT Tutorials/T17.java
@@ -0,0 +1,15 @@
+public class T17 {
+    public static void main(String[] args){
+        // Nested Loop Logic
+
+        int i;
+        int j;
+
+        for(i = 0; i < 10; i++){
+            for(j = 0; j < 3; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
