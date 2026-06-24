8f2d6a151305768011d1e331cfc47618a10fd77f
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-25 02:45:34 +0800

    OOP: LOGIC & FUNCTIONS

---
+++ YT Tutorials/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2025;
+    double price = 5000000;
+    boolean isRunning = false;
+
+    void start(){
+        isRunning = true;
+        System.out.println("You start the engine");
+    }
+    void stop(){
+        isRunning = false;
+        System.out.println("You stop the engine");
+    }
+}
---
+++ YT Tutorials/T26.java
@@ -0,0 +1,20 @@
+public class T26 {
+    public static void main(String[] args){
+        // OOP: LOGIC & FUNCTIONS
+
+        Car car = new Car();
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+        car.start();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+
+
+
+    }
+}
