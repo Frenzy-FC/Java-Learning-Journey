99ef49b901ba6bd336e07ff747dfa7bef95f5400
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-28 23:30:21 +0800

    Add T32: Super Keyword: Logic & Functions

---
+++ YT Tutorials/T32.java
@@ -0,0 +1,11 @@
+public class T32 {
+    public static void main(String[] args){
+        // Super Keyword: Logic & Functions
+
+        Person person = new Person("Frenz", "Cuarteros");
+        Employee employee = new Employee("Frenz", "Cuarteros", 1000);
+
+        employee.showSalary();
+        person.showName();
+    }
+}
