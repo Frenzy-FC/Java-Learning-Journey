e721f1b8d9f87aeb97dc2420a05a528bd89db0e0
Frenz <cuarteros.frenzcarlo123@gmail.com>
2026-06-28 22:47:14 +0800

    Add T30: Static Logic & Function

---
+++ YT Tutorials/T30.java
@@ -0,0 +1,10 @@
+public class T30 {
+    public static void main(String[] args){
+
+        Friend friend1 = new Friend("Frenz");
+        Friend friend2 = new Friend("Carlo");
+        Friend friend3 = new Friend("Cuarteros");
+        
+        Friend.showFriend();
+    }
+}
